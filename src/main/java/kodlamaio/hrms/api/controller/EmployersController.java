0f73c2255ae9f807f49a24c69ff930f2a46ea0fb
EZGI <EZGI@LAPTOP-EL3H4BU3>
2023-03-15 23:05:02 +0300

    7.gün ödevi

---
+++ src/main/java/kodlamaio/hrms/api/controller/EmployersController.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import kodlamaio.hrms.business.abstracts.EmployerService;
+
+import kodlamaio.hrms.entities.concretes.Employer;
+
+@RestController
+@RequestMapping("api/employeers")
+public class EmployersController {
+
+	private EmployerService employerService;
+
+	@Autowired
+	public EmployersController(EmployerService employerService) {
+		super();
+		this.employerService = employerService;
+	}
+	
+	@GetMapping("/getall")
+	public List<Employer> getAll(){
+		return this.employerService.getAll();
+	}
+	
+}
--- src/main/java/kodlamaio/hrms/core/utilities/results/ErrorResult.java
+++ src/main/java/kodlamaio/hrms/core/utilities/results/ErrorResult.java
@@ -1,5 +1,14 @@
 package kodlamaio.hrms.core.utilities.results;
 
-public class ErrorResult {
+public class ErrorResult extends Result {
+
+	public ErrorResult( ) {
+		super(false);
+	}
+	
+	public ErrorResult(String message ) {
+		super(false,message);
+	}
+	
 
 }
