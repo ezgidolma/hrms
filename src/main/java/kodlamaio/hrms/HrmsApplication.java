25b1979c134803d9e7165967bc277cf297fa6ca1
ezgidolma <ezgidolma23@gmail.com>
2023-05-01 15:55:11 +0300

    .

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,7 +16,10 @@ public class HrmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
+
 	
+
+		
 	@Bean
 	public Docket api() {                
 	    return new Docket(DocumentationType.SWAGGER_2)          
